4d02fc98d3e120c90e8255fd226decfb0a817086
penpiter <66522294+penpiter@users.noreply.github.com>
2020-11-09 02:25:26 +0100

    Update test4.ps1

--- test4.ps1
+++ test4.ps1
@@ -1,15 +1,16 @@
 $ID=get-content c:\Windows\id
 $GITPath="https://raw.githubusercontent.com/penpiter/wds/main/lockscreen_"+$ID+".jpg"
 $statusCodePS = Invoke-WebRequest -Uri $GITPath | % {$_.StatusCode}
+echo $GITPath
 
  if($statusCodePS -eq 200) {
     wget $GITPath -OutFile .\lockscreen.jpg
-	 
+	echo $statusCodePS
+   
 	$lsimg =  (ls lockscreen.jpg).DirectoryName+"\lockscreen.jpg"
-	.\Set-Screen.ps1 -LockScreenSource "$lsimg"
+	#.\Set-Screen.ps1 -LockScreenSource "$lsimg"
     .\LockScreenImage.ps1 -LockScreenImageSource "$lsimg"
  }
 
 
  rundll32.exe user32.dll, UpdatePerUserSystemParameters
- 
