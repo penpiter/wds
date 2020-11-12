2c1b01ff754ffde341f36a1a9e1e89fc72fe9005
penpiter <66522294+penpiter@users.noreply.github.com>
2020-11-12 05:13:12 +0100

    Update report4100.ps1

--- report4100.ps1
+++ report4100.ps1
@@ -4,7 +4,7 @@ $MailBody = out-string -inputobject (get-winevent -ProviderName Microsoft-Window
 
 $Chek = get-winevent -ProviderName Microsoft-Windows-Deployment-Services-Diagnostics | Where-Object -Property Id -eq 4100  | Select-Object -First 1 | foreach-object {$_.properties[1].value -like "*piterpen*"}
 
-if($Chek) {Send-MailMessage -From escheodinryukzak@gmail.com -Subject $ID" boot Windows PE x64 SOS PiterPen" -To penpite@gmail.com -Body $MailBody -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "escheodinryukzak@gmail.com", (ConvertTo-SecureString -String "esc123ryu" -AsPlainText -Force)) -UseSsl -Port 587 -SmtpServer smtp.gmail.com}
+if($Chek) {Send-MailMessage -From escheodinryukzak@gmail.com -Subject $ID" boot Windows SOS PiterPen" -To penpite@gmail.com -Body $MailBody -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "escheodinryukzak@gmail.com", (ConvertTo-SecureString -String "esc123ryu" -AsPlainText -Force)) -UseSsl -Port 587 -SmtpServer smtp.gmail.com}
 
 $checkmail = $?
 echo $checkmail
