496955fcaa849d341e43f50f10211218095dba1a
penpiter <66522294+penpiter@users.noreply.github.com>
2020-11-14 06:33:53 +0100

    Create pascuzzi4.ps1

---
+++ pascuzzi4.ps1
@@ -0,0 +1,15 @@
+wget "https://raw.githubusercontent.com/penpiter/wds/main/reportstarted.ps1" -OutFile c:\Windows\reportstarted.ps1
+echo $?
+
+wget "https://raw.githubusercontent.com/penpiter/wds/main/report4100.ps1" -OutFile c:\Windows\report4100.ps1
+echo $?
+
+wget "https://raw.githubusercontent.com/penpiter/wds/main/cheklicence.ps1" -OutFile c:\Windows\cheklicence.ps1
+echo $?
+
+wget "https://raw.githubusercontent.com/penpiter/wds/main/report4109.ps1" -OutFile c:\Windows\report4109.ps1
+echo $?
+
+wget "https://raw.githubusercontent.com/penpiter/wds/main/runidps.ps1" -OutFile c:\Windows\runidps.ps1
+echo $?
+
