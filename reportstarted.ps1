7b7e60842e7dcf7aa24aaa4440a6f039c2196024
penpiter <66522294+penpiter@users.noreply.github.com>
2020-11-14 10:05:44 +0100

    Delete reportstarted.ps1

--- reportstarted.ps1
+++
@@ -1,3 +0,0 @@
-$ID=get-content c:\Windows\id
-
-Send-MailMessage -From escheodinryukzak@gmail.com -Subject $ID" WDS started" -To penpite@gmail.com -Body $ID" WDS started" -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "escheodinryukzak@gmail.com", (ConvertTo-SecureString -String "esc123ryu" -AsPlainText -Force)) -UseSsl -Port 587 -SmtpServer smtp.gmail.com
